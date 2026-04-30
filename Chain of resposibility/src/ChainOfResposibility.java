6ca27fe7522c49233088376b1b6163e9c83e2339
Carlos Silva <carlos110eduardo@gmail.com>
2026-04-29 23:31:59 -0300

    criado o arquivo inicial do padrão chain of resposibility

---
+++ Chain of resposibility/src/ChainOfResposibility.java
@@ -0,0 +1,26 @@
+class Pedido {
+    private double valor;
+    private boolean aprovado;
+
+    public Pedido(double valor){
+        this.valor = valor;
+        this.aprovado = false;
+    }
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+    public void aprovar() {
+        this.aprovado = true;
+    }
+    public void cancelar(){
+        this.aprovado = false;
+    }
+    
+}
+
+
+public class ChainOfResposibility{
+    public static void main(String[] args){
+
+    }
+}
