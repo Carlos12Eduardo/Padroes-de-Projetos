82de682426ead04d19675590d34fe8559362c455
Carlos Silva <carlos110eduardo@gmail.com>
2026-05-30 23:59:36 -0300

    adicinodo no estado de Trancado a opçao de mudar para em análise

--- State/src/Trancado.java
+++ State/src/Trancado.java
@@ -2,4 +2,10 @@ public class Trancado extends MatriculaState {
     public Trancado() {
         super.setDescricao("Trancado");
     }
+    
+    @Override
+    public MatriculaState analisar() {
+        System.out.println("Matricula atualizada para em análise.");
+        return new EmAnalise();
+    }
 }
